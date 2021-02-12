2c9dcd36ebc890dba5e91b29fdb8f8b8ec5c1f05
Christian Evensen <h584988@stud.hvl.no>
2021-02-12 13:26:47 +0100

    Jeg har laget address

--- src/dataModels/Adress.java
+++ src/dataModels/Adress.java
@@ -2,4 +2,39 @@ package dataModels;
 
 public class Adress {
 
+	private String streetAddress;
+	private int postalCode;
+	private String postPlace;
+	
+	
+	public Adress() {
+		
+	}
+	
+	public Adress(String streetAddress, int postalCode, String postPlace) {
+		this.streetAddress = streetAddress;
+		this.postalCode = postalCode;
+		this.postPlace = postPlace;
+	}
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	public int getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getPostPlace() {
+		return postPlace;
+	}
+	public void setPostPlace(String postPlace) {
+		this.postPlace = postPlace;
+	}
+	
+	
+	
 }
